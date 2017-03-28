0e7b35b95da2477c7122e5d9562364a3ab03e46c
Anuchit <anoochit@gmail.com>
2017-03-28 15:55:57 +0700

    check request

--- web/index.php
+++ web/index.php
@@ -5,9 +5,10 @@ require_once '../vendor/autoload.php';
 //POST
 $input = file_get_contents('php://input');
 $json = json_decode($input);
+$event = $json->events[0];
 
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('rHESonstdV3FniND+25WHjgb+mTxTOeikt7zCGV9h0LgBFzCprU8IQwrXpgreO2TGyewjB87Imwmyz/RbxqF2N/i7mA+gMnmaS2cTF5GKLozTwE25XEHvRiaCtL74zveIKzb/UVfbaU4poxxB3WcrQdB04t89/1O/w1cDnyilFU=');
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => 'e8726b9bfd344a12c189f4fc6512da8a']);
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient("rHESonstdV3FniND+25WHjgb+mTxTOeikt7zCGV9h0LgBFzCprU8IQwrXpgreO2TGyewjB87Imwmyz/RbxqF2N/i7mA+gMnmaS2cTF5GKLozTwE25XEHvRiaCtL74zveIKzb/UVfbaU4poxxB3WcrQdB04t89/1O/w1cDnyilFU=");
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => "e8726b9bfd344a12c189f4fc6512da8a"]);
 
 if ("message" == $event->type) {      
     if ("text" == $event->message->type) {
