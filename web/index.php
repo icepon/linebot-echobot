0f05bb5a0b92b31688122e68ae0448c9b33953ff
Anuchit Chalothorn <anoochit@gmail.com>
2017-04-13 01:15:55 +0700

    add demo message to test message template

--- web/index.php
+++ web/index.php
@@ -9,29 +9,13 @@ use Monolog\Handler\FirePHPHandler;
 $logger = new Logger('linebot');
 $logger->pushHandler(new StreamHandler('php://stderr', Logger::DEBUG));
 
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('rHESonstdV3FniND+25WHjgb+mTxTOeikt7zCGV9h0LgBFzCprU8IQwrXpgreO2TGyewjB87Imwmyz/RbxqF2N/i7mA+gMnmaS2cTF5GKLozTwE25XEHvRiaCtL74zveIKzb/UVfbaU4poxxB3WcrQdB04t89/1O/w1cDnyilFU=');
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => 'e8726b9bfd344a12c189f4fc6512da8a']);
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('YOUR_LINEBOT_ACCESS_TOKEN');
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => 'YOUR_LINEBOT_CHANNEL_SECRET']);
 
 $signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
 try {
   $events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
-  
-  foreach ($events as $event) {
-	if (!($event instanceof MessageEvent)) {
-		$logger->info('Non message event has come');
-		continue;
-	}
-	if (!($event instanceof TextMessage)) {
-		$logger->info('Non text message has come');
-		continue;
-	}
-	$replyText = $event->getText();
-	$logger->info('Reply text: ' . $replyText);
-	$resp = $bot->replyText($event->getReplyToken(), $replyText);
-	$logger->info($resp->getHTTPStatus() . ': ' . $resp->getRawBody());
-  }
-
-
+  $logger->info(var_dump($events));
 } catch(\LINE\LINEBot\Exception\InvalidSignatureException $e) {
   error_log('parseEventRequest failed. InvalidSignatureException => '.var_export($e, true));
 } catch(\LINE\LINEBot\Exception\UnknownEventTypeException $e) {
@@ -41,4 +25,3 @@ try {
 } catch(\LINE\LINEBot\Exception\InvalidEventRequestException $e) {
   error_log('parseEventRequest failed. InvalidEventRequestException => '.var_export($e, true));
 }
-
