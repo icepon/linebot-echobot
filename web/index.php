57eb280d95313b0a006cb662ef63c5223bffc8da
Anuchit Chalothorn <anoochit@gmail.com>
2017-04-13 00:28:50 +0700

    add demo message to test message template

--- web/index.php
+++ web/index.php
@@ -1,27 +1,17 @@
-<?php
+// Loging
+$logger = new Logger('linebot');
+$logger->pushHandler(new StreamHandler('php://stderr', Logger::DEBUG));
 
-require_once '../vendor/autoload.php';
+$bot = new \LINE\LINEBot (
+   new \LINE\LINEBot\HTTPClient\CurlHTTPClient("rHESonstdV3FniND+25WHjgb+mTxTOeikt7zCGV9h0LgBFzCprU8IQwrXpgreO2TGyewjB87Imwmyz/RbxqF2N/i7mA+gMnmaS2cTF5GKLozTwE25XEHvRiaCtL74zveIKzb/UVfbaU4poxxB3WcrQdB04t89/1O/w1cDnyilFU="),
+   ['ChannelSecret' => "e8726b9bfd344a12c189f4fc6512da8a"]
+ );
 
-use Monolog\Logger;
-use Monolog\Handler\StreamHandler;
-use Monolog\Handler\FirePHPHandler;
-
-$signature = $request->headers->get(HTTPHeader::LINE_SIGNATURE);
-
-$bot = new LINEBot(new CurlHTTPClient('rHESonstdV3FniND+25WHjgb+mTxTOeikt7zCGV9h0LgBFzCprU8IQwrXpgreO2TGyewjB87Imwmyz/RbxqF2N/i7mA+gMnmaS2cTF5GKLozTwE25XEHvRiaCtL74zveIKzb/UVfbaU4poxxB3WcrQdB04t89/1O/w1cDnyilFU='), [
-            'channelSecret' => 'e8726b9bfd344a12c189f4fc6512da8a',
-        ]);
+$signature = $_SERVER["HTTP_". \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
+$body = file_get_contents("php://input");
 try {
-    $events = $bot->parseEventRequest($request->getContent(), $signature);
-	$message_events = [];
-	foreach ($events as $event) {
-    	if (!($event instanceof MessageEvent) && !($event instanceof PostbackEvent)) {
-        	continue;
-    	}
-    $message_events[] = $event;
-	$logger->info(print_r($event,true));
-	}
-
+	$events = $bot->parseEventRequest($body, $signature);
 } catch (Exception $e) {
-	$logger->info($e->getMessage());
-}
+   var_dump($ e); 
+} 
+
