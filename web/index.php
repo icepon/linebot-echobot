515e1f397afd0020c70e46ccc49c84ae4bb864db
Anuchit Chalothorn <anoochit@gmail.com>
2017-04-13 00:18:16 +0700

    add demo message to test message template

--- web/index.php
+++ web/index.php
@@ -6,73 +6,22 @@ use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 use Monolog\Handler\FirePHPHandler;
 
-// Loging
-$logger = new Logger('linebot');
-$logger->pushHandler(new StreamHandler('php://stderr', Logger::DEBUG));
+$signature = $request->headers->get(HTTPHeader::LINE_SIGNATURE);
 
-//POST
-$input = file_get_contents('php://input');
-$json = json_decode($input);
-$event = $json->events[0];
-$logger->info(print_r($event, true));
+$bot = new LINEBot(new CurlHTTPClient('rHESonstdV3FniND+25WHjgb+mTxTOeikt7zCGV9h0LgBFzCprU8IQwrXpgreO2TGyewjB87Imwmyz/RbxqF2N/i7mA+gMnmaS2cTF5GKLozTwE25XEHvRiaCtL74zveIKzb/UVfbaU4poxxB3WcrQdB04t89/1O/w1cDnyilFU='), [
+            'channelSecret' => 'e8726b9bfd344a12c189f4fc6512da8a',
+        ]);
+try {
+    $events = $bot->parseEventRequest($request->getContent(), $signature);
+	$message_events = [];
+	foreach ($events as $event) {
+    	if (!($event instanceof MessageEvent) && !($event instanceof PostbackEvent)) {
+        	continue;
+    	}
+    $message_events[] = $event;
+	$logger->info(print_r($event,true));
+	}
 
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient("rHESonstdV3FniND+25WHjgb+mTxTOeikt7zCGV9h0LgBFzCprU8IQwrXpgreO2TGyewjB87Imwmyz/RbxqF2N/i7mA+gMnmaS2cTF5GKLozTwE25XEHvRiaCtL74zveIKzb/UVfbaU4poxxB3WcrQdB04t89/1O/w1cDnyilFU=");
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => "e8726b9bfd344a12c189f4fc6512da8a"]);
-
-if ("message" == $event->type) {      
-    if ("text" == $event->message->type) {
-		$message_text=strtolower(trim($event->message->text));
-		switch ($message_text) {
-			case "text" : 
-				$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("text message");
-				break;
-			case "location" :
-				$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\LocationMessageBuilder("Eiffel Tower", "Champ de Mars, 5 Avenue Anatole France, 75007 Paris, France", 48.858328, 2.294750);
-				break;
-			case "button" :
-				$actions = array (
-   					// general message action
-   					New \LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder("button 1", "text 1"),
-   					// URL type action
-   					New \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder("Google", "http://www.google.com"),
-   					// The following two are interactive actions
-   					New \LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder("next page", "page=3"),
-   					New \LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder("Previous", "page=1")
- 				);
-				$img_url = "https://cdn.shopify.com/s/files/1/0379/7669/products/sampleset2_1024x1024.JPG?v=1458740363";
- 				$button = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\ButtonTemplateBuilder("button text", "description", $img_url, $actions);
- 				$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder("this message to use the phone to look to the Oh", $button);
-				break;
-			case "confirm" :
-				$actions = array (
-   					New \LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder("yes", "ans=y"),
-   					New \LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder("no", "ans=N")
- 				);
- 				$button = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\ConfirmTemplateBuilder("problem", $actions);
- 				$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder("this message to use the phone to look to the Oh", $button);
-				 break;
-			default :
-				$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("demo command: text, location, button, confirm to test message template");	
-				break;
- 
-		}
-/*		if ($event->message->text=="t1") {
-			$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("ข้อความช่วยเหลือ");
-		} else {
-			$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("พิมพ์ ? เพื่อขอความช่วยเหลือ");
-		}
-*/
-    } else {
-        $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder(":)");
-		$logger->info('message type is ' . $event->message->type);
-    }
-} else if("postback"==$event->type) {
-	$logger->info('postback recieve');
-} else {
-	$logger->info('event type is not message -->'.$event->type);
+} catch (Exception $e) {
+	$logger->info($e->getMessage());
 }
-
-$response = $bot->replyMessage($event->replyToken, $textMessageBuilder);
-$logger->info(print_r($event->replyToken, true));
-$logger->info(print_r($response, true));
-return;
