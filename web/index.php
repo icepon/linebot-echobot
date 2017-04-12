e6e7e4287e0416bb4e038c231d7c99a17af519a9
Anuchit Chalothorn <anoochit@gmail.com>
2017-04-13 01:03:52 +0700

    add demo message to test message template

--- web/index.php
+++ web/index.php
@@ -9,10 +9,8 @@ use Monolog\Handler\FirePHPHandler;
 $logger = new Logger('linebot');
 $logger->pushHandler(new StreamHandler('php://stderr', Logger::DEBUG));
 
-$bot = new \LINE\LINEBot (
-   new \LINE\LINEBot\HTTPClient\CurlHTTPClient("rHESonstdV3FniND+25WHjgb+mTxTOeikt7zCGV9h0LgBFzCprU8IQwrXpgreO2TGyewjB87Imwmyz/RbxqF2N/i7mA+gMnmaS2cTF5GKLozTwE25XEHvRiaCtL74zveIKzb/UVfbaU4poxxB3WcrQdB04t89/1O/w1cDnyilFU="),
-   ['ChannelSecret' => "e8726b9bfd344a12c189f4fc6512da8a"]
- );
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('rHESonstdV3FniND+25WHjgb+mTxTOeikt7zCGV9h0LgBFzCprU8IQwrXpgreO2TGyewjB87Imwmyz/RbxqF2N/i7mA+gMnmaS2cTF5GKLozTwE25XEHvRiaCtL74zveIKzb/UVfbaU4poxxB3WcrQdB04t89/1O/w1cDnyilFU=');
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => 'e8726b9bfd344a12c189f4fc6512da8a']);
 
 $signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
 try {
@@ -26,4 +24,3 @@ try {
 } catch(\LINE\LINEBot\Exception\InvalidEventRequestException $e) {
   error_log('parseEventRequest failed. InvalidEventRequestException => '.var_export($e, true));
 }
-
