7f6a7c131936616f5f01f7c2ad18381a4189265f
icepon <ice_pon@hotmail.com>
2017-09-23 20:52:55 +0700

    Update index.php

--- web/index.php
+++ web/index.php
@@ -6,4 +6,4 @@ require_once '../vendor/autoload.php';
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($_ENV["LINEBOT_ACCESS_TOKEN"]);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $_ENV["LINEBOT_CHANNEL_SECRET"]]);
 echo "1.1";
-//echo "1.2";
+echo "1.2";
