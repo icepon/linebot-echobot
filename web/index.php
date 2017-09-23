b8d6cdf1c29866ab44d49ba209f732d6198ecf43
icepon <ice_pon@hotmail.com>
2017-09-23 21:50:20 +0700

    Update index.php

--- web/index.php
+++ web/index.php
@@ -7,4 +7,6 @@ $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($_ENV["LINEBOT_ACCESS_
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $_ENV["LINEBOT_CHANNEL_SECRET"]]);
 echo "1.1";
 echo "1.2";
-$response = $bot->replyText('<reply token>', 'hello!');
+$replyToken = $event['replyToken'];
+$response = $bot->replyText($replyToken, "hello!");
+echo "1.3";
