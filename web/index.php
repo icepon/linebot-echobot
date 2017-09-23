0e397cf1e1c5fa5d87b3b16b42c6f2a0eb6aeaea
icepon <ice_pon@hotmail.com>
2017-09-23 21:55:30 +0700

    Update index.php

--- web/index.php
+++ web/index.php
@@ -13,7 +13,7 @@ echo "1.3";
 */
 
 
-$access_token = $_ENV["LINEBOT_ACCESS_TOKEN"];
+$access_token = 'AdlGlZFCmua0+/PYr/y/iI7dF2c7DeVXkhG/FKp9K4Pp8qAuEWTv7yAx7vDX1t3B31gVTyIwIMhyO4g1XWptfVyFJ7kmUGdDrfB4Pd/UspZp0iIMrLeVq+YJIV0ZY0arNIDv4eVmwLOCm2yns5ezewdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
