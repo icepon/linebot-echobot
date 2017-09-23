a36c86188c0581ab828c84b903467acc875378d9
icepon <ice_pon@hotmail.com>
2017-09-23 21:58:09 +0700

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,12 +1,12 @@
 <?php
-/*
+
 require_once '../vendor/autoload.php';
 
 
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($_ENV["LINEBOT_ACCESS_TOKEN"]);
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $_ENV["LINEBOT_CHANNEL_SECRET"]]);
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('AdlGlZFCmua0+/PYr/y/iI7dF2c7DeVXkhG/FKp9K4Pp8qAuEWTv7yAx7vDX1t3B31gVTyIwIMhyO4g1XWptfVyFJ7kmUGdDrfB4Pd/UspZp0iIMrLeVq+YJIV0ZY0arNIDv4eVmwLOCm2yns5ezewdB04t89/1O/w1cDnyilFU=');
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '1efe3c53bbd84320ce616b832c262094']);
 echo "1.1";
-echo "1.2";
+/*echo "1.2";
 $replyToken = $event['replyToken'];
 $response = $bot->replyText($replyToken, "hello!");
 echo "1.3";
