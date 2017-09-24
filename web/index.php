7227f2e4303fd6366b9fec775a669e66d0a79b5c
icepon <ice_pon@hotmail.com>
2017-09-25 00:06:49 +0700

    Update index.php

--- web/index.php
+++ web/index.php
@@ -66,12 +66,6 @@ if (!is_null($events['events'])) {
 			//$messages = [ 'type'=>'text','text'=>$kin[rand(0, count($kin) - 1)]]; 
 			//$outputText = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($messages);	
 			//$response = $bot->replyMessage($replyToken, $outputText); 
-			$actions = array (
-				New \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder("ตรวจสอบข้อมูล", "https://awa.aia.co.th/login/")
-			);
-			$b_description = "คุณพงศธร T123456789 MEMO 31/01/61";	
-			$button = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\ButtonTemplateBuilder(null, $b_description,null,  $actions);
-			$outputText = new \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder($b_description, $button);
 			$response = $bot->replyMessage($replyToken, $outputText);
 			}
 			//#NB1
