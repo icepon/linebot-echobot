69254a0b295929ffc86383eaeaca5cf2217da530
icepon <ice_pon@hotmail.com>
2017-09-24 20:58:07 +0700

    Update index.php

--- web/index.php
+++ web/index.php
@@ -5,7 +5,12 @@ require_once '../vendor/autoload.php';
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('AdlGlZFCmua0+/PYr/y/iI7dF2c7DeVXkhG/FKp9K4Pp8qAuEWTv7yAx7vDX1t3B31gVTyIwIMhyO4g1XWptfVyFJ7kmUGdDrfB4Pd/UspZp0iIMrLeVq+YJIV0ZY0arNIDv4eVmwLOCm2yns5ezewdB04t89/1O/w1cDnyilFU=');
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '1efe3c53bbd84320ce616b832c262094']);
-echo "1.1";
+echo '<span>",
+'<script type="text/javascript" src="//media.line.me/js/line-button.js?v=20140411" ></script>',
+'<script type="text/javascript">',
+'new media_line_me.LineButton({"pc":false,"lang":"en","type":"a"});',
+'</script>',
+'</span>';
 /*echo "1.2";
 $replyToken = $event['replyToken'];
 $response = $bot->replyText($replyToken, "hello!");
