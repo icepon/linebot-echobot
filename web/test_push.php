ec74a1575a666158c119c6de1a0bcd8e02389b48
icepon <ice_pon@hotmail.com>
2017-09-25 11:18:27 +0700

    Update test_push.php

--- web/test_push.php
+++ web/test_push.php
@@ -9,3 +9,6 @@ $access_token = 'AdlGlZFCmua0+/PYr/y/iI7dF2c7DeVXkhG/FKp9K4Pp8qAuEWTv7yAx7vDX1t3
 echo  $_GET["userId"];
 echo  $_GET["m_type"];
 echo  $_GET["m_text"];
+
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($_GET["m_text"]);
+$response = $bot->pushMessage($_GET["userId"], $textMessageBuilder);
