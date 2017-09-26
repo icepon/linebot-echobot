9b48e98a72e8f0be2fc0c38829f7ab6d97d7c112
icepon <ice_pon@hotmail.com>
2017-09-26 09:40:48 +0700

    Update index.php

--- web/index.php
+++ web/index.php
@@ -158,7 +158,7 @@ if (!is_null($events['events'])) {
 			//$response = $bot->replyMessage($replyToken, $outputText);
 			
 			//Carousel
-			$columns = array();
+			/*$columns = array();
 			$actions1 = array(
 			    	New \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder("Barcode จ่ายเงิน", "http://iaia-uat.aia.co.th/pay?p=T690321979&a=8197890&d=020714"),
 				New \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder("ส่งต่อข้อความ", "https://lineit.line.me/share/ui?url=%e0%b8%84%e0%b8%b8%e0%b8%93%e0%b8%9e%e0%b8%87%e0%b8%a8%e0%b8%98%e0%b8%a3+T123456789+%e0%b8%84%e0%b8%a3%e0%b8%9a%e0%b8%81%e0%b8%b3%e0%b8%ab%e0%b8%99%e0%b8%94+31%2f01%2f61+%e0%b8%88%e0%b8%b3%e0%b8%99%e0%b8%a7%e0%b8%99+50%2c000+%e0%b8%9a%e0%b8%b2%e0%b8%97+http%3A%2F%2Fiaia-uat.aia.co.th%2Fpay%3Fp%3dT690321979%26a%3d8197890%26d%3d020714"),
@@ -178,7 +178,19 @@ if (!is_null($events['events'])) {
 			  array_push($columns,$column2);	
 			
 			$carousel = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselTemplateBuilder($columns);
-			$outputText = new \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder($b_description1, $carousel);
+			$outputText = new \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder($b_description1, $carousel);*/
+				$columns = array();
+$img_url = "https://cdn.shopify.com/s/files/1/0379/7669/products/sampleset2_1024x1024.JPG?v=1458740363";
+for($i=0;$i<5;$i++) {
+  $actions = array(
+    new \LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder("Add to Cart","action=carousel&button=".$i),
+    new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder("View","http://www.google.com")
+  );
+  $column = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumnTemplateBuilder("Title", "description", $img_url , $actions);
+  $columns[] = $column;
+}
+$carousel = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselTemplateBuilder($columns);
+$outputText = new \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder("Carousel Demo", $carousel);
 			$response = $bot->replyMessage($replyToken, $outputText);
 			}
 			else if (strtolower($text) == "bill2") {
