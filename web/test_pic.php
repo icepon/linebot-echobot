950351ca6d70f78c61ee0ff28d895b40948036ca
icepon <ice_pon@hotmail.com>
2017-10-05 17:36:48 +0700

    Create test_pic.php

---
+++ web/test_pic.php
@@ -0,0 +1,30 @@
+<?php
+function LoadJpeg($imgname)
+{
+    /* Attempt to open */
+    $im = @imagecreatefromjpeg($imgname);
+
+    /* See if it failed */
+    if(!$im)
+    {
+        /* Create a black image */
+        $im  = imagecreatetruecolor(150, 30);
+        $bgc = imagecolorallocate($im, 255, 255, 255);
+        $tc  = imagecolorallocate($im, 0, 0, 0);
+
+        imagefilledrectangle($im, 0, 0, 150, 30, $bgc);
+
+        /* Output an error message */
+        imagestring($im, 1, 5, 5, 'Error loading ' . $imgname, $tc);
+    }
+
+    return $im;
+}
+
+header('Content-Type: image/jpeg');
+$img_url = "https://img.wongnai.com/p/s/2016/06/29/81e4c5272de045c1a68834aea8ba9ee0.jpg";
+$img = LoadJpeg($img_url);
+
+imagejpeg($img);
+imagedestroy($img);
+?>
