ae9615258cbc5e2dfd6a02fccfad7837cb006857
icepon <ice_pon@hotmail.com>
2017-10-05 17:46:49 +0700

    Update test_push.php

--- web/test_push.php
+++ web/test_push.php
@@ -13,24 +13,24 @@ echo  $_GET["m_text"];
 //Image builder
 function LoadJpeg($imgname)
 {
-    /* Attempt to open */
+    /*
     $im = @imagecreatefromjpeg($imgname);
 
-    /* See if it failed */
+   
     if(!$im)
     {
-        /* Create a black image */
+        
         $im  = imagecreatetruecolor(150, 30);
         $bgc = imagecolorallocate($im, 255, 255, 255);
         $tc  = imagecolorallocate($im, 0, 0, 0);
 
         imagefilledrectangle($im, 0, 0, 150, 30, $bgc);
 
-        /* Output an error message */
+        
         imagestring($im, 1, 5, 5, 'Error loading ' . $imgname, $tc);
     }
-
-    return $im;
+*/
+    return $imgname;
 }
 
 
