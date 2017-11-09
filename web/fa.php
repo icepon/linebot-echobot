fae1fd8d0532acfedf60dc923c7811d298f6018d
icepon <ice_pon@hotmail.com>
2017-11-09 16:11:53 +0700

    Update fa.php

--- web/fa.php
+++ web/fa.php
@@ -2,7 +2,7 @@
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('AdlGlZFCmua0+/PYr/y/iI7dF2c7DeVXkhG/FKp9K4Pp8qAuEWTv7yAx7vDX1t3B31gVTyIwIMhyO4g1XWptfVyFJ7kmUGdDrfB4Pd/UspZp0iIMrLeVq+YJIV0ZY0arNIDv4eVmwLOCm2yns5ezewdB04t89/1O/w1cDnyilFU=');
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '1efe3c53bbd84320ce616b832c262094']);
 $access_token = 'AdlGlZFCmua0+/PYr/y/iI7dF2c7DeVXkhG/FKp9K4Pp8qAuEWTv7yAx7vDX1t3B31gVTyIwIMhyO4g1XWptfVyFJ7kmUGdDrfB4Pd/UspZp0iIMrLeVq+YJIV0ZY0arNIDv4eVmwLOCm2yns5ezewdB04t89/1O/w1cDnyilFU=';
-
+echo 'A1';
 1userid = "Ub67a42c2b7fbb4a11e1962cd6f3e6036";
 iuserid = "U9f0c0e8e2ad753d0067fd7c3f9ab644e";
 
