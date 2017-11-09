b5b401e322a26dc305093c7c40955a785419cc7b
icepon <ice_pon@hotmail.com>
2017-11-09 16:39:35 +0700

    Update testtt.php

--- web/testtt.php
+++ web/testtt.php
@@ -2,8 +2,8 @@
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('AdlGlZFCmua0+/PYr/y/iI7dF2c7DeVXkhG/FKp9K4Pp8qAuEWTv7yAx7vDX1t3B31gVTyIwIMhyO4g1XWptfVyFJ7kmUGdDrfB4Pd/UspZp0iIMrLeVq+YJIV0ZY0arNIDv4eVmwLOCm2yns5ezewdB04t89/1O/w1cDnyilFU=');
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '1efe3c53bbd84320ce616b832c262094']);
 $access_token = 'AdlGlZFCmua0+/PYr/y/iI7dF2c7DeVXkhG/FKp9K4Pp8qAuEWTv7yAx7vDX1t3B31gVTyIwIMhyO4g1XWptfVyFJ7kmUGdDrfB4Pd/UspZp0iIMrLeVq+YJIV0ZY0arNIDv4eVmwLOCm2yns5ezewdB04t89/1O/w1cDnyilFU=';
-puserid = "Ub67a42c2b7fbb4a11e1962cd6f3e6036";
-iuserid = "U9f0c0e8e2ad753d0067fd7c3f9ab644e";
+$puserid = "Ub67a42c2b7fbb4a11e1962cd6f3e6036";
+$iuserid = "U9f0c0e8e2ad753d0067fd7c3f9ab644e";
 
 $m_type = $_POST['action'];
 
@@ -28,7 +28,7 @@ if (strtolower($m_type) == "inb_approve") {
 			$carousel = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselTemplateBuilder($columns);
 					
 			$outputText = new \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder("แจ้งเตือนสถานะกรมธรรม์ใหม่", $carousel);
-			$response = $bot->pushMessage(iuserid, $outputText);
+			$response = $bot->pushMessage($iuserid, $outputText);
 }
 
 /*
