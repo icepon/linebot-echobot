19f536105d6e3a0989fbe01948545407229dc721
icepon <ice_pon@hotmail.com>
2017-11-09 17:15:22 +0700

    Update testtt.php

--- web/testtt.php
+++ web/testtt.php
@@ -2,7 +2,8 @@
 require_once '../vendor/autoload.php';
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('AdlGlZFCmua0+/PYr/y/iI7dF2c7DeVXkhG/FKp9K4Pp8qAuEWTv7yAx7vDX1t3B31gVTyIwIMhyO4g1XWptfVyFJ7kmUGdDrfB4Pd/UspZp0iIMrLeVq+YJIV0ZY0arNIDv4eVmwLOCm2yns5ezewdB04t89/1O/w1cDnyilFU=');
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '1efe3c53bbd84320ce616b832c262094']);
-
+$1user = "Ub67a42c2b7fbb4a11e1962cd6f3e6036";
+$iceuser = "U9f0c0e8e2ad753d0067fd7c3f9ab644e";
 
 $access_token = 'AdlGlZFCmua0+/PYr/y/iI7dF2c7DeVXkhG/FKp9K4Pp8qAuEWTv7yAx7vDX1t3B31gVTyIwIMhyO4g1XWptfVyFJ7kmUGdDrfB4Pd/UspZp0iIMrLeVq+YJIV0ZY0arNIDv4eVmwLOCm2yns5ezewdB04t89/1O/w1cDnyilFU=';
 
@@ -51,10 +52,10 @@ else if (strtolower($m_type) == "inb_approve") {
 			$carousel = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselTemplateBuilder($columns);
 					
 			$outputText = new \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder("แจ้งเตือนสถานะกรมธรรม์ใหม่", $carousel);
-			$response = $bot->pushMessage("U9f0c0e8e2ad753d0067fd7c3f9ab644e", $outputText);	}
+			$response = $bot->pushMessage($iceuser, $outputText);	}
 
 else
 {
 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($m_type);
-$response = $bot->pushMessage("U9f0c0e8e2ad753d0067fd7c3f9ab644e", $textMessageBuilder);
+$response = $bot->pushMessage($iceuser, $textMessageBuilder);
 }
