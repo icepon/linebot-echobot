9f094391846e57011a1f34e0b7058c0912a0258b
icepon <ice_pon@hotmail.com>
2017-12-19 15:01:18 +0700

    Update index.php

--- web/index.php
+++ web/index.php
@@ -94,7 +94,7 @@ if (!is_null($events['events'])) {
 			$outputText = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($user_id);	
 			$response = $bot->replyMessage($replyToken, $outputText); }
 			else if ($text == "ไอซ์") {
-				sleep(15);
+				sleep(59);
 			$outputText = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("จ๋าาาาาาา");	
 			$response = $bot->replyMessage($replyToken, $outputText); }
 			else if ($text == "กินไรดี2") {
