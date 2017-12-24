dcf8f4e6e1caf49048a5816ff77700c4fd642407
icepon <ice_pon@hotmail.com>
2017-12-24 12:49:50 +0700

    Update index.php

--- web/index.php
+++ web/index.php
@@ -96,11 +96,11 @@ if (!is_null($events['events'])) {
     $result = trim( curl_exec( $ch ) );
     curl_close($ch);
     // Responce the result.
-	$result2 =	json_encode($result, JSON_UNESCAPED_UNICODE);
-    $decode = json_decode($result2, true);
+	//$result2 =	json_encode($result, JSON_UNESCAPED_UNICODE);
+    $decode = json_decode($result, true);
 		
    
-			$outputText = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($decode['intents']['intent']);	
+			$outputText = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($decode);	
 			$response = $bot->replyMessage($replyToken, $outputText);
 			//end watson
 			
