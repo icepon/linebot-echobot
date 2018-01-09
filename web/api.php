2aecaf3a423c243311540ccc4a7d2170e8a596c2
icepon <ice_pon@hotmail.com>
2018-01-09 14:04:12 +0700

    Update api.php

--- web/api.php
+++ web/api.php
@@ -16,7 +16,7 @@ include 'segment.php'; //ตัดคำ
     if (!empty($text_)) {
     $segment = new Segment();
     
-    $result2 = $segment->get_segment_array("หิวข้าวจัง");
+    //$result2 = $segment->get_segment_array("หิวข้าวจัง");
     //$text2 = implode(' ', $result2);
     $data['input']['text'] = "หิวข้าวจัง";
     }
