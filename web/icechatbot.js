44c125c03e99992e320986407f0c18802260efde
icepon <ice_pon@hotmail.com>
2018-01-12 17:38:24 +0700

    Update icechatbot.js

--- web/icechatbot.js
+++ web/icechatbot.js
@@ -50,7 +50,7 @@ function chatbot(message){
       // Upodate the conversation state.
       context = JSON.stringify(JSON.parse(response).context);
       intent = JSON.parse(response).intents[0];
-      console.log(intent);
+      console.log(JSON.parse(intent).intent);
       keeplog(JSON.parse(response).context.conversation_id,"1","2",JSON.parse(response).input.text);
     }
   }).fail(function () {
