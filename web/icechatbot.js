1b8d35b4497ec91b2731b697286cb8e45d75ce76
icepon <ice_pon@hotmail.com>
2018-01-22 18:45:17 +0700

    Update icechatbot.js

--- web/icechatbot.js
+++ web/icechatbot.js
@@ -62,7 +62,7 @@ function chatbot(message){
 
 function markfalse(){
   var jsondata = {"is_correct": 'N'};
-  var url = str1.concat("https://icechatbot-a7be.restdb.io/rest/chatlog/",msgid);
+  var url = string.concat("https://icechatbot-a7be.restdb.io/rest/chatlog/",msgid);
 var settings = {
   "async": true,
   "crossDomain": true,
