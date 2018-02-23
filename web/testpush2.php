9351ef1a017d29474205056662d9fbba5c5b2da1
icepon <ice_pon@hotmail.com>
2018-02-23 16:08:33 +0700

    Update testpush2.php

--- web/testpush2.php
+++ web/testpush2.php
@@ -36,11 +36,11 @@ else
     //$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder(checkRichmenuOfUser($access_token, $_GET["userId"]));
     //$response = $bot->pushMessage($_GET["userId"], $textMessageBuilder);
 	//Create Rich menu
-    $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder(createNewRichmenu($access_token));
-    $response = $bot->pushMessage($_GET["userId"], $textMessageBuilder);
-	//Map rich menu
-    //$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder(linkToUser($access_token,$_GET["userId"],'richmenu-65e627cedfff38f7b53676039c0370e1'));
+    //$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder(createNewRichmenu($access_token));
     //$response = $bot->pushMessage($_GET["userId"], $textMessageBuilder);
+	//Map rich menu
+    $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder(linkToUser($access_token,$_GET["userId"],'richmenu-b098de523f9ac25bd70446981bf833ae'));
+    $response = $bot->pushMessage($_GET["userId"], $textMessageBuilder);
 	
 }
 
