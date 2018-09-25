c153a61fc9fcca61b38a248366ae5b1df1865bd5
icepon <ice_pon@hotmail.com>
2018-09-25 14:52:30 +0700

    Update index.php

--- web/index.php
+++ web/index.php
@@ -206,31 +206,29 @@ if (!is_null($events['events'])) {
 			else if(strpos( $text , "เอ๋" ) !== false){
 			$outputText = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("เอ๋สวย");	
 			$response = $bot->replyMessage($replyToken, $outputText); }
-			else if ($text == "สาธุ") {
-			curl -X POST \
--H 'Content-Type:application/json' \
--H 'Authorization: Bearer $access_token' \
--d '{
-     "replyToken":$replyToken,
-     "messages":[
-          {
-               "type":"text",
-               "text":"Hello, user"
-          },
-          {
-               "type":"text",
-               "text":"May I help you?"
-          }
-      ]
-}' https://api.line.me/v2/bot/message/reply
 			
+			else if(strpos( $text , "สาธุ" ) !== false){
+			$outputText = "testttt";
+			$messages = [
+				'type' => 'text',
+				'text' => $outputText
+				];
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = [
+			'replyToken' => $replyToken,
+			'messages' => [$messages],
+			];
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
 			}
-			
-			
-			
-			
-			
-			
 			
 			else if ($text == "ชวนเพื่อนสมัคร") {
 			//$outputText = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("มาสมัครกันเถอะ");
