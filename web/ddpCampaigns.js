51b23bc9f712b4d5f4ed605deba532d38426805c
icepon <ice_pon@hotmail.com>
2021-01-06 15:17:46 +0700

    Add files via upload

---
+++ web/ddpCampaigns.js
@@ -0,0 +1,18 @@
+var jsonObject = {
+	"campaignList" : [
+	{
+		"campaignId":"c0001",
+		"campaignName":"New year 2020",
+		"campaignCode":"happynewyear",
+		"campaignFrom":"2020-01-01",
+		"campaignTo":"2020-12-31",
+	},
+	{
+		"campaignId":"c0002",
+		"campaignName":"Songkarn 2020",
+		"campaignCode":"songkarn2020",
+		"campaignFrom":"2020-01-01",
+		"campaignTo":"2020-04-31",
+	}
+	]
+}
